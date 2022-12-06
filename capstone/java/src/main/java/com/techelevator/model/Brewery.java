80e4cde40cb6ce516e7fb68c1f71e4bf1ca4efef
Tolalope Oshikanlu <tolalopeoshikanlu@gmail.com>
2022-12-06 12:22:09 -0500

    pt1

---
+++ capstone/java/src/main/java/com/techelevator/model/Brewery.java
@@ -0,0 +1,75 @@
+package com.techelevator.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Brewery {
+
+    private String id;
+    private String name;
+    private String street;
+    private String city;
+    private String state;
+    private String phone;
+
+    @JsonProperty ("website_url")
+    private String url;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+
+
+}
