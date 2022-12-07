f5c8e4e94ccb8b2d0c070936cc450f872be79e4c
Dolly Lorn <dollylorn102907@gmail.com>
2022-12-07 15:35:01 -0800

    DA PROJECT

--- capstone/vue/src/views/Login.vue
+++ capstone/vue/src/views/Login.vue
@@ -1,45 +1,59 @@
 <template>
-  <div id="login" class="text-center">
-    <form class="form-signin" @submit.prevent="login">
-      <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
-      <div
-        class="alert alert-danger"
-        role="alert"
-        v-if="invalidCredentials"
-      >Invalid username and password!</div>
-      <div
-        class="alert alert-success"
-        role="alert"
-        v-if="this.$route.query.registration"
-      >Thank you for registering, please sign in.</div>
-      <label for="username" class="sr-only">Username</label>
-      <input
-        type="text"
-        id="username"
-        class="form-control"
-        placeholder="Username"
-        v-model="user.username"
-        required
-        autofocus
-      />
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      <router-link :to="{ name: 'register' }">Need an account?</router-link>
-      <button type="submit">Sign in</button>
-    </form>
-  </div>
+  <body>
+    <div id="login" class="text-center">
+      <form class="form-signin" @submit.prevent="login">
+        <h1>Bender BeerFinder</h1>
+        <h2> It's A Match!</h2>
+        <div class="alert alert-danger" role="alert" v-if="invalidCredentials">
+          Invalid username and password!
+        </div>
+        <div
+          class="alert alert-success"
+          role="alert"
+          v-if="this.$route.query.registration"
+        >
+          Thank you for registering, please sign in.
+        </div>
+
+        <div id="username">
+          <label for="username" class="sr-only"></label>
+
+          <input
+            type="text"
+            id="username"
+            class="form-control"
+            placeholder="Username"
+            v-model="user.username"
+            required
+            autofocus
+          />
+        </div>
+
+        <div id="password">
+          <label for="password" class="sr-only"></label>
+
+          <input
+            type="password"
+            id="password"
+            class="form-control"
+            placeholder="Password"
+            v-model="user.password"
+            required
+          />
+        </div>
+        <router-link id="create-account" :to="{ name: 'register' }"
+          >Create Account</router-link
+        >
+
+        <button id="sign-in" type="submit">Sign in</button>
+      </form>
+    </div>
+
+  </body>
 </template>
 
 <script>
 import authService from "../services/AuthService";
-
 export default {
   name: "login",
   components: {},
@@ -47,30 +61,103 @@ export default {
     return {
       user: {
         username: "",
-        password: ""
+        password: "",
+        role: "user",
       },
-      invalidCredentials: false
+      invalidCredentials: false,
     };
   },
   methods: {
     login() {
       authService
         .login(this.user)
-        .then(response => {
+        .then((response) => {
           if (response.status == 200) {
             this.$store.commit("SET_AUTH_TOKEN", response.data.token);
             this.$store.commit("SET_USER", response.data.user);
-            this.$router.push("/");
+            console.log(response.data.user.authorities[0].name);
+            if (response.data.user.authorities[0].name == "ROLE_BREWER") {
+              this.$router.push({ name: "breweries" });
+            } else if (
+              response.data.user.authorities[0].name == "ROLE_BEERLOVER"){
+              this.$router.push({ name: "beerlover" });
+            } else {
+              this.$router.push("/home");
+            }
           }
         })
-        .catch(error => {
+        .catch((error) => {
           const response = error.response;
-
           if (response.status === 401) {
             this.invalidCredentials = true;
           }
         });
-    }
-  }
+    },
+  },
 };
 </script>
+
+<style scoped>
+
+input::placeholder {
+  color: black;
+}
+* {
+  padding: 0;
+  margin: 0;
+}
+input {
+  display: block;
+  height: 50px;
+  width: 50vw;
+  background-color: rgba(255, 255, 255, 0.829);
+  border: 2px solid rgba(0, 0, 0, 0.383);
+  border-radius: 3px;
+  padding: 0 10px;
+  margin-top: 4px;
+  font-size: 17px;
+  font-weight: 500;
+  text-align: center;
+  color: black;
+  
+ 
+}
+body {
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+
+}
+body,
+html {
+  height: 100vh;
+}
+h1 {
+  font-weight: bolder;
+  margin-bottom: 5%;
+  font-size: 300%;
+  color: darkgoldenrod;
+}
+h2 {
+  font-weight: bolder;
+  margin-bottom: 15%;
+  color: darkgoldenrod;
+}
+#sign-in {
+  margin-top: 2%;
+  width: 51.3vw;
+  height: 6vh;
+  border: 2px solid rgba(255, 255, 255, 0.1);
+  color: darkgoldenrod;
+  font-size: 130%;
+  font-weight: bold;
+  background-color: black;
+  cursor: pointer;
+}
+#login.text-center {
+  width: 50vw;
+}
+
+</style>
